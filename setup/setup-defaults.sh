6c64132ecd7f79442c95941af702e1bcbc3d9990
Caleb Perkins <calebperkins@users.noreply.github.com>
2017-03-25 22:20:58 -0700

    More Finder settings.

--- setup/setup-defaults.sh
+++ setup/setup-defaults.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+function CFPreferencesAppSynchronize() {
+    python - <<END
+from Foundation import CFPreferencesAppSynchronize
+CFPreferencesAppSynchronize('$1')
+END
+}
+
 # ==============================================
 # Finder
 # ==============================================
@@ -11,7 +18,20 @@ defaults write com.apple.finder AppleShowAllExtensions -bool true
 # New window points to home
 defaults write com.apple.finder NewWindowTarget -string "PfHm"
 
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Disable the warning when removing from iCloud Drive
+defaults write com.apple.finder FXEnableRemoveFromICloudDriveWarning -bool false
+
+# delete items in trash after 30 days automatically
+defaults write com.apple.finder FXRemoveOldTrashItems -bool true
+
 CFPreferencesAppSynchronize "com.apple.finder"
+
 # ==============================================
 # Terminal
 # ==============================================
@@ -21,3 +41,21 @@ echo "Setting Terminal preferences"
 defaults write com.apple.Terminal Shell -string "/bin/zsh"
 
 CFPreferencesAppSynchronize "com.apple.Terminal"
+
+# ==============================================
+# Safari & WebKit
+# ==============================================
+echo "Setting Safari & WebKit preferences"
+
+# Appearance
+
+# Show status bar
+defaults write com.apple.Safari ShowStatusBar -bool true
+
+# Do not track
+defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+
+# Don't open "safe" files after downloading
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+CFPreferencesAppSynchronize "com.apple.Safari"
